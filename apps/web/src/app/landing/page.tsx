169bdc85b9aec69c00727eced82c6edb2201fc36
Ishika14097 <ghoshishika63@gmail.com>
2026-06-06 11:36:53 +0530

    self host video

--- apps/web/src/app/landing/page.tsx
+++ apps/web/src/app/landing/page.tsx
@@ -26,7 +26,7 @@ export default function LandingPage() {
         className="absolute inset-0 w-full h-full object-cover"
       >
         <source
-          src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260508_215831_c6a8989c-d716-4d8d-8745-e972a2eec711.mp4"
+          src="/videos/background.mp4"
           type="video/mp4"
         />
       </video>
